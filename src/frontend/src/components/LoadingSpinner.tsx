48486552e6dfaac0793d38f4afe35be1ccafea1b
Antonio Coppe <46423374+AntonioCoppe@users.noreply.github.com>
2025-06-15 08:52:27 -0400

    Replace spinner with skyline progress animation

--- src/frontend/src/components/LoadingSpinner.tsx
+++ src/frontend/src/components/LoadingSpinner.tsx
@@ -7,7 +7,11 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ text }) => (
   <div className="loading-spinner">
-    <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="Loading" />
+    <div className="skyline-wrapper">
+      <div className="skyline-progress">
+        <img src={process.env.PUBLIC_URL + '/Skyline.svg'} alt="Loading" />
+      </div>
+    </div>
     {text && <div className="loading-text">{text}</div>}
   </div>
 );
