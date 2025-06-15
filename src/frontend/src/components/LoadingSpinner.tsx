a4f4a90b6e6195f47aa0634a601621884b993eb9
Antonio Coppe <46423374+AntonioCoppe@users.noreply.github.com>
2025-06-15 08:57:21 -0400

    Improve skyline loading animation

--- src/frontend/src/components/LoadingSpinner.tsx
+++ src/frontend/src/components/LoadingSpinner.tsx
@@ -8,9 +8,8 @@ interface LoadingSpinnerProps {
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ text }) => (
   <div className="loading-spinner">
     <div className="skyline-wrapper">
-      <div className="skyline-progress">
-        <img src={process.env.PUBLIC_URL + '/Skyline.svg'} alt="Loading" />
-      </div>
+      <img src={process.env.PUBLIC_URL + '/Skyline.svg'} alt="Loading" />
+      <div className="skyline-mask" />
     </div>
     {text && <div className="loading-text">{text}</div>}
   </div>
